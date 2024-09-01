9c3d0f56ea70d097b9e5cca6ea33c2a767c409a4
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-01 14:38:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-teal-400'>
+      <div className=' bg-sky-500 text-white'>
+        App
+      </div>
+    </div>
+  )
+}
+
+export default App
